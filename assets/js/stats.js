6992c684148de3f0498bc4fb879a45e0156a0837
rlmarsh85 <rlmarsh85@gmail.com>
2021-01-18 15:23:59 -0700

    Adding colors for pie chart

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -18,16 +18,20 @@ jQuery(document).ready(function() {
     // append the svg object to the div called 'my_dataviz'
     var svg = d3.select("#placeholder")
     .append("svg")
-        .attr("width", width)
-        .attr("height", height)
+    .attr("width", width)
+    .attr("height", height)
     .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
     
     // set the color scale
     var color = d3.scaleOrdinal()
     .domain(data)
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
+    // red, green, blue, off-black, off-white
+    //orange, teal, purple, yellow, pink
+    //.range(["#ff3300", "#79f304", "#0936fb", "#303134", "#cfd1db"])
+    //.range(["#f1a20a", "#0af1d4", "#c60af1", "#d4da10", "#f10d96"])
+    .range(["#ff3300", "#79f304", "#0936fb", "#303134", "#cfd1db", "#f1a20a", "#0af1d4", "#c60af1", "#d4da10", "#f10d96"])
 
     // Compute the position of each group on the pie:
     var pie = d3.pie()
@@ -49,7 +53,7 @@ jQuery(document).ready(function() {
       .attr('fill', function(d){ return(color(d.data.key)) })
       .attr("stroke", "black")
       .style("stroke-width", "2px")
-      .style("opacity", 0.7)
+      .style("opacity", 0.8)
 
     svg
     .selectAll('whatever')
