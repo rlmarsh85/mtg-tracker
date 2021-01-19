6a84f7dff2f6c87fddb87b119f3cfec05345d2a9
rlmarsh85 <rlmarsh85@gmail.com>
2021-01-18 17:33:55 -0700

    Improving pie chart

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,7 +1,9 @@
 var d3 = require('d3');
+var d3legend = require('d3-svg-legend');
+
 //console.log(total_games);
 jQuery(document).ready(function() {
-    console.log(total_games);
+
     // Create dummy data
     var data = player_data;
 
@@ -18,30 +20,32 @@ jQuery(document).ready(function() {
     // append the svg object to the div called 'my_dataviz'
     var svg = d3.select("#placeholder")
     .append("svg")
-    .attr("width", width)
+    .attr("width", width + 200)
     .attr("height", height)
     .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    
+    // red, green, blue, off-black, off-white
+    //orange, teal, purple, yellow, pink    
+    //var colorRange = ["#ff3300", "#79f304", "#0936fb", "#303134", "#cfd1db"];
+    //var colorRange = ["#f1a20a", "#0af1d4", "#c60af1", "#d4da10", "#f10d96"];
+    var colorRange = ["#ff3300", "#79f304", "#0936fb", "#303134", "#cfd1db", "#f1a20a", "#0af1d4", "#c60af1", "#d4da10", "#f10d96"];
     // set the color scale
     var color = d3.scaleOrdinal()
     .domain(data)
-    // red, green, blue, off-black, off-white
-    //orange, teal, purple, yellow, pink
-    //.range(["#ff3300", "#79f304", "#0936fb", "#303134", "#cfd1db"])
-    //.range(["#f1a20a", "#0af1d4", "#c60af1", "#d4da10", "#f10d96"])
-    .range(["#ff3300", "#79f304", "#0936fb", "#303134", "#cfd1db", "#f1a20a", "#0af1d4", "#c60af1", "#d4da10", "#f10d96"])
+    .range(colorRange)
 
     // Compute the position of each group on the pie:
     var pie = d3.pie()
-    .value(function(d) {console.log(d.value);return d.value;})
-    var data_ready = pie(d3.entries(data))
+    .value(function(d) { return d.value.WinRatio })
 
+    var data_ready = pie(d3.entries(data))
     var arcGenerator = d3.arc()
     .innerRadius(0)
     .outerRadius(radius)
 
+    console.log("Data ready:");
+    console.log(data_ready);
     // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
 
     svg
@@ -60,10 +64,56 @@ jQuery(document).ready(function() {
     .data(data_ready)
     .enter()
     .append('text')
-    .text(function(d){ return d.data.key + d.value + "%" })
+    .text(function(d){ return  d.data.value.WinRatio + "%" })
     .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
     .style("text-anchor", "middle")
     .style("font-size", 17)
+
+    svg
+    .selectAll('whatever')
+    .data(data_ready)
+    .enter()
+    .append('text')
+    .text(function(d){ return "(" + (d.data.value.NumWins) + " Games)"})
+    .attr("transform", function(d) { var centroid = arcGenerator.centroid(d);
+                                     var x = centroid[0];
+                                     var y = centroid[1] + 20;
+                                     return "translate(" + x + "," + y + ")";  
+                                    })
+    .style("text-anchor", "middle")
+    .style("font-size", 14)    
+
+
+    var ordinal = d3.scaleOrdinal()
+    .domain(Object.keys(player_data))
+    .range(colorRange);
+  
+    var legendXPos = width / 2;
+    var legendYPos = (height /3) * -1;
+
+    svg.append("rect")
+    .attr("class", "legendBorder")
+    .attr("transform", "translate(" + (legendXPos -10) + "," + (legendYPos -20) + ")")
+    
+
+    svg.append("g")
+    .attr("class", "legendOrdinal")
+    .attr("transform", "translate(" + legendXPos + "," + legendYPos + ")")
+    ;
+
+    var legendOrdinal = d3legend.legendColor()
+    .shape("path", d3.symbol().type(d3.symbolSquare).size(150)())
+    .shapePadding(10)
+    .scale(ordinal)
+    
+
+    svg.select(".legendOrdinal")
+    .call(legendOrdinal);
+
+    svg.select("rect")
+    .attr("height", (svg.select("g").node().getBBox().height + 20))
+    .attr("width", (svg.select("g").node().getBBox().width + 20))
+    .attr("style", "outline: thin solid black;fill:white")   //This will do the job
 
 
     
